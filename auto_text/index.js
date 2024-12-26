2bc530401f15bec9f6bbb81129f96223d8c3aa29
nursahtuncel <nursahtuncell@gmail.com>
2024-12-27 01:44:59 +0300

    folders creeated

---
+++ auto_text/index.js
@@ -0,0 +1,8 @@
+const dataCount = document.querySelectorAll(".auto_text");
+console.log(dataCount);
+let texts = [];
+let reverse = false;
+let idx = 0;
+let txtNum = 0;
+let speed = 200;
+let waitAtEnd = 2;
