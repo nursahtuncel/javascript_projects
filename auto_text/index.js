948de94d852946d095949fbdfcf2ade3327debf7
nursahtuncel <nursahtuncell@gmail.com>
2024-12-27 08:50:21 +0300

    done

--- auto_text/index.js
+++ auto_text/index.js
@@ -6,3 +6,22 @@ let idx = 0;
 let txtNum = 0;
 let speed = 200;
 let waitAtEnd = 2;
+dataCount.forEach((text) => {
+  let data = text.getAttribute("data");
+  texts.push(data);
+  console.log(data);
+});
+let write = setInterval(() => {
+  if (idx == 0 || (idx < texts[txtNum].length + waitAtEnd && !reverse)) {
+    reverse = false;
+    dataCount[0].innerHTML = texts[txtNum].toString().slice(0, idx);
+    idx++;
+  } else {
+    reverse = true;
+    dataCount[0].innerHTML = texts[txtNum].toString().slice(0, idx);
+    idx--;
+    if (idx == 0) {
+      txtNum == texts.length - 1 ? (txtNum = 0) : txtNum++;
+    }
+  }
+}, speed);
